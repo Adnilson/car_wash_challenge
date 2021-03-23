5e33b08da33a2a5a2a63abbb6f7f6cda2bcaeba0
Adnilson <adnilson.delgado@outlook.com>
2021-03-23 16:02:31 +0000

    Use different license_plate from fixtures

--- test/models/car_test.rb
+++ test/models/car_test.rb
@@ -6,12 +6,12 @@ class CarTest < ActiveSupport::TestCase
   end
 
   test "Fields presence validations" do
-    car = Car.new(year: 2021, color: 'blue', license_plate: '33-DS-22', price: 320, model: @model)
-    car2 = Car.new(color: 'blue', license_plate: 'PT', price: 320, model: @model)
-    car3 = Car.new(year: 2021, license_plate: 'PT', price: 320, model: @model)
+    car = Car.new(year: 2021, color: 'blue', license_plate: '37-DS-22', price: 320, model: @model)
+    car2 = Car.new(color: 'blue', license_plate: '17-DS-22', price: 320, model: @model)
+    car3 = Car.new(year: 2021, license_plate: '17-DS-22', price: 320, model: @model)
     car4 = Car.new(year: 2021, color: 'blue', price: 320, model: @model)
-    car5 = Car.new(year: 2021, color: 'blue', license_plate: 'PT', model: @model)
-    car6 = Car.new(year: 2021, color: 'blue', license_plate: 'PT', price: 320)
+    car5 = Car.new(year: 2021, color: 'blue', license_plate: '17-DS-22', model: @model)
+    car6 = Car.new(year: 2021, color: 'blue', license_plate: '17-DS-22', price: 320)
 
     assert car.save
     assert_not car2.save
@@ -22,14 +22,14 @@ class CarTest < ActiveSupport::TestCase
   end
 
   test "license_plate uniqueness validation" do
-    Car.create(year: 2021, color: 'blue', license_plate: 'PT', price: 320, model: @model)
-    second = Car.new(year: 2021, color: 'blue', license_plate: 'PT', price: 320, model: @model)
+    Car.create(year: 2021, color: 'blue', license_plate: '17-DS-22', price: 320, model: @model)
+    second = Car.new(year: 2021, color: 'blue', license_plate: '17-DS-22', price: 320, model: @model)
 
     assert_not second.save
   end
 
   test "license_plate format validation" do
-    car = Car.new(year: 2021, color: 'blue', license_plate: '33-DS-22', price: 320, model: @model)
+    car = Car.new(year: 2021, color: 'blue', license_plate: '37-DS-22', price: 320, model: @model)
     car2 = Car.new(year: 2021, color: 'blue', license_plate: '', price: 320, model: @model)
     car3 = Car.new(year: 2021, color: 'blue', license_plate: 'DS-DS-22', price: 320, model: @model)
     car4 = Car.new(year: 2021, color: 'blue', license_plate: 'PT', price: 320, model: @model)
